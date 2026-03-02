42f33834b32de4ece5f50c5925454a94ebdfbfec
Eduardo Reynaga Cobos <eduardo.reynagacobos@unidemat.edu.mx>
2026-03-02 16:59:13 -0600

    Se agrego la nueva rama de la camara

--- Vision_ESP32-CAM/include/ProtocoloEspNow.h
+++ Vision_ESP32-CAM/include/ProtocoloEspNow.h
@@ -1,5 +1,6 @@
 #ifndef PROTOCOLO_ESPNOW_H
 #define PROTOCOLO_ESPNOW_H
+
 #include <Arduino.h>
 
 typedef struct MensajeVision {
@@ -7,8 +8,10 @@ typedef struct MensajeVision {
     int16_t coordX;           
     int16_t coordY;           
     float distanciaEstimada;  
-    bool porteriaEnemigaAlineada; 
+    bool porteriaEnemigaAlineada; // TRUE si vemos la porteria azul/amarilla
 } MensajeVision;
 
+// MAC Address of the MAIN ESP32 (Replace 0xFF later!)
 const uint8_t direccionMacCerebro[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
 #endif
