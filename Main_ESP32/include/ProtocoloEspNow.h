5147ff2df52a49caeaceb7406f9380cf9f23ad89
Eduardo Reynaga Cobos <eduardo.reynagacobos@unidemat.edu.mx>
2026-03-16 13:56:58 -0500

    test cambio

--- Main_ESP32/include/ProtocoloEspNow.h
+++ Main_ESP32/include/ProtocoloEspNow.h
@@ -10,5 +10,7 @@ typedef struct MensajeVision {
     bool porteriaEnemigaAlineada; 
 } MensajeVision;
 
+// Se tuvo que cambiar la direccion MAC de su direccion raw a hexadecimal
+// Se tiene que usar la misma direccion en la CAM tambien
 const uint8_t direccionMacCerebro[] = {0x00, 0x70, 0x07, 0x1C, 0x0F, 0xB0};
 #endif
