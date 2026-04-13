1320f4d40255c32a6b205d39974f224bf8c6c6bd
Eduardo Reynaga Cobos <eduardo.reynagacobos@unidemat.edu.mx>
2026-04-13 10:48:23 -0500

    nuevos cambios

--- Main_ESP32/include/ProtocoloEspNow.h
+++ Main_ESP32/include/ProtocoloEspNow.h
@@ -5,8 +5,8 @@
 // =========================================================================
 // 1. EL INTERRUPTOR DE ROBOTS (Cambia esto antes de subir el código)
 // =========================================================================
-#define ROBOT_A  // <- Deja esto así para programar el Cerebro del Robot A
-// #define ROBOT_B // <- Quita las barras (//) para programar el Cerebro del Robot B
+// #define ROBOT_A  // <- Deja esto así para programar el Cerebro del Robot A
+#define ROBOT_B // <- Quita las barras (//) para programar el Cerebro del Robot B
 
 // =========================================================================
 // 2. LA ESTRUCTURA DE DATOS (Universal para ambos robots)
@@ -29,9 +29,8 @@ typedef struct MensajeVision {
   const uint8_t direccionMacCamara[]  = {0x88, 0x57, 0x21, 0xC2, 0x0B, 0x68};
 
 #elif defined(ROBOT_B)
-  // Direcciones para el Robot B
-  // NOTA: Usa la MAC de la Cámara B que acabas de encontrar en el monitor
-  const uint8_t direccionMacCerebro[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00}; // Falta descubrir Cerebro B
+  // La MAC del Cerebro B (¡Descubierta con el cable hackeado!)
+  const uint8_t direccionMacCerebro[] = {0x00, 0x70, 0x07, 0x1C, 0xA0, 0x84}; 
   const uint8_t direccionMacCamara[]  = {0x88, 0x57, 0x21, 0xC3, 0x44, 0x58};
 
 #else
--- Main_ESP32/src/main.cpp
+++ Main_ESP32/src/main.cpp
@@ -1,52 +1,43 @@
-#include <WiFi.h>
+// Este es el archivo principal del programa
+// Aquí se inicializan todos los subsistemas y se ejecuta el ciclo principal del robot
+#include <Arduino.h>
+#include "Motores.h"
+#include "Sensores.h"
+#include "Odometria.h"
+#include "ControlPID.h"
+#include "Comunicacion.h"
+#include "Estrategia.h"
+
 void setup() {
-  Serial.begin(115200);
-  WiFi.mode(WIFI_STA);
-  Serial.println();
-  Serial.print("MAC ESP32: ");
-  Serial.println(WiFi.macAddress());
+    Serial.begin(115200);
+    
+    // Inicializar todos los subsistemas físicos de forma ordenada
+    inicializarMotores(); // Este modulo vive en Motores.cpp
+    inicializarBusI2C(); // Este modulo vive en Sensores.cpp
+    inicializarIMU_BNO055(); // Este modulo vive en Sensores.cpp
+    inicializarToF_VL53L0X(); // Este modulo vive en Sensores.cpp
+    inicializarLinea_QTR8A(); // Este modulo vive en Sensores.cpp
+    
+    // Inicializar los subsistemas de inteligencia y red
+    inicializarOdometria(); // Este modulo vive en Odometria.cpp
+    inicializarPID(); // Este modulo vive en ControlPID.cpp
+    inicializarRadio(); // Este modulo vive en Comunicacion.cpp
+    inicializarEstrategia(); // Este modulo vive en Estrategia.cpp
+    
+    // Estos println solamente son para verificar que se este ejecutando el setup correctamente
+    // Luego se puede eliminar o comentar para ahorrar recursos
+    Serial.println("Robot Defensor Listo! Esperando...");
 }
-void loop() {}
-// // Este es el archivo principal del programa
-// // Aquí se inicializan todos los subsistemas y se ejecuta el ciclo principal del robot
-// #include <Arduino.h>
-// #include "Motores.h"
-// #include "Sensores.h"
-// #include "Odometria.h"
-// #include "ControlPID.h"
-// #include "Comunicacion.h"
-// #include "Estrategia.h"
 
-// void setup() {
-//     Serial.begin(115200);
+void loop() {
+    // 1. Actualizar el GPS interno (saber dónde estamos)
+    actualizarPosicion();
     
-//     // Inicializar todos los subsistemas físicos de forma ordenada
-//     inicializarMotores(); // Este modulo vive en Motores.cpp
-//     inicializarBusI2C(); // Este modulo vive en Sensores.cpp
-//     inicializarIMU_BNO055(); // Este modulo vive en Sensores.cpp
-//     inicializarToF_VL53L0X(); // Este modulo vive en Sensores.cpp
-//     inicializarLinea_QTR8A(); // Este modulo vive en Sensores.cpp
+    // 2. El director técnico piensa (lee sensores y cámara)
+    evaluarEntorno();
     
-//     // Inicializar los subsistemas de inteligencia y red
-//     inicializarOdometria(); // Este modulo vive en Odometria.cpp
-//     inicializarPID(); // Este modulo vive en ControlPID.cpp
-//     inicializarRadio(); // Este modulo vive en Comunicacion.cpp
-//     inicializarEstrategia(); // Este modulo vive en Estrategia.cpp
+    // 3. El director técnico actúa (mueve los motores)
+    ejecutarJugadaActual();
     
-//     // Estos println solamente son para verificar que se este ejecutando el setup correctamente
-//     // Luego se puede eliminar o comentar para ahorrar recursos
-//     Serial.println("Robot Defensor Listo! Esperando...");
-// }
-
-// void loop() {
-//     // 1. Actualizar el GPS interno (saber dónde estamos)
-//     actualizarPosicion();
-    
-//     // 2. El director técnico piensa (lee sensores y cámara)
-//     evaluarEntorno();
-    
-//     // 3. El director técnico actúa (mueve los motores)
-//     ejecutarJugadaActual();
-    
-//     delay(10); // Pequeña pausa para darle estabilidad al procesador
-// }
+    delay(10); // Pequeña pausa para darle estabilidad al procesador
+}
